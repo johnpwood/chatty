f4a4db662de1ec9ea02917932b2bec73af21fb9f
john <johnpwoodiv@gmail.com>
2017-09-19 15:07:03 -0500

    app is operational

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
+'use strict'
 
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const app = express()
+
+app.use(express.static('assets'))
+app.use(bodyParser.json())
+
+let messages = [];
+
+app.get('/messages', function(req, res, next) {
+  res.status(200).json({ messages: messages})
+})
+
+app.post('/messages', function(req, res, next) {
+messages.push({ message: req.body.message, time: new Date() });
+  res.status(200).json({ messages: messages})
+})
+
+
+
+
+
+
+
+app.listen(3000)
